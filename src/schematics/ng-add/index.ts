6efbe3ac2c2e427d9023ff1dda49dfc9b2661018
Ruslan Lekhman <lekhman112@gmail.com>
2021-12-06 15:56:20 -0700

    feat: log output

--- src/schematics/ng-add/index.ts
+++ src/schematics/ng-add/index.ts
@@ -33,22 +33,45 @@ function updateAngularConfig(): Rule {
   const spinner = ora({
     text: `Updating workspace builders...`,
     // Workaround for https://github.com/sindresorhus/ora/issues/136.
-    discardStdin: process.platform != 'win32'
+    discardStdin: process.platform !== 'win32'
   }).start();
   return updateWorkspace((workspace) => {
-    const targets = Array.from(allWorkspaceTargets(workspace));
+    const targets = Array.from(allWorkspaceTargets(workspace)).map(([, target]) => target);
+    const updatedTargets: string[] = [ ];
+    const notUpdatedTargets: string[] = [ ];
     // eslint-disable-next-line max-len
-    const regexp = /^@angular-devkit\/build-angular:(browser|dev-server|extract-i18n|karma|server)$/g;
-    for (const [, target] of targets) {
+    const regexp = /^@angular-devkit\/build-angular:(browser|dev-server|extract-i18n|karma|server)$/;
+    for (const target of targets) {
       if (regexp.test(target.builder)) {
         target.builder = target.builder
           .replace('@angular-devkit/build-angular', 'ngx-pug-builders');
+        updatedTargets.push(target.builder);
+      } else {
+        notUpdatedTargets.push(target.builder);
       }
     }
-    spinner.succeed(
+    spinner.succeed([
       // eslint-disable-next-line max-len
-      'Workspace builders successfully updated from @angular-devkit/build-angular:* to ngx-pug-builders:*.'
-    );
+      'Workspace builders successfully updated from @angular-devkit/build-angular:* to ngx-pug-builders:*.',
+      ...(
+        updatedTargets.length
+          ? [
+            '',
+            `Updated targets (${updatedTargets.length}): `,
+            ...updatedTargets.map((target) => `✔ ${target}`)
+          ]
+          : [ ]
+      ),
+      ...(
+        notUpdatedTargets.length
+          ? [
+            '',
+            `Not updated targets (${notUpdatedTargets.length}): `,
+            ...notUpdatedTargets.map((target) => `ℹ️ ${target}`)
+          ]
+          : [ ]
+      )
+    ].join('\n'));
     spinner.stop();
   });
 }
